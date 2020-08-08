406866d05746815c404ad8a4ab2fa70f1e827253
viniciusleonardovky <vile.pb@gmail.com>
2020-08-08 00:15:48 -0300

    fix database name

--- backend/src/config/mongo.ts
+++ backend/src/config/mongo.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/formare', {
+mongoose.connect('mongodb://localhost:27017/chat', {
   useNewUrlParser: true,
   useFindAndModify: true,
   useUnifiedTopology: true,
